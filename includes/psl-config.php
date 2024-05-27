ab63e1299d4af52f242b816b31c08eb53867eb5d
Fabricio171 <fabricioaltafiniteodo@gmail.com>
2024-05-27 17:17:01 -0300

    nova atividade

---
+++ includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+    /*logim para o banco de dados*/
+    
+    define("host", "localhost"); /*localizar com host que vc quer conectar*/ 
+    define("user", "sec_user"); /*nome do usuario do banco de dados*/
+    define("password", "4Fa98xkHVd2XmnfK"); /*senha do banco*/
+    define("database", "tutocrudphp") //nome do banco
+    
+    define("can_register", "any");
+    define("default_role", "member");
+
+    define("secure", False); // ESTRITAMENTE PARA DESENVOLVIMENTO!!!!
+
